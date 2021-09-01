adbce10ccc14054e02ebdd50a1a16d6c97f57a5a
subarna <subarnaakter1210@gmail.com>
2021-09-02 02:29:53 +0600

    updated

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
-const searchBook=()=>{
-    const searchField=document.getElementById('search-field');
-     const searchText=searchField.value;
-     searchField.value='';
-
+    
+    
+const searchBook=async()=>{
+  const searchField=document.getElementById('search-field');
+  const searchText=searchField.value;
+    searchField.value='';
     const url=`https://openlibrary.org/search.json?q=${searchText}`;
-
-    fetch(url)
+  
+     fetch(url)
     .then(res=>res.json())
-    .then(data=>displayBook(data.docs))
+    .then(data=>displayBook(data.docs.slice(0,30))) 
 }
 
    
 const displayBook=books=>{
     const bookContainer=document.getElementById('book-container');
-for(const book of books){
-
-    const div=document.createElement('div');
-    div.innerHTML=`
-                <div class="col shadow-lg rounded">
-                  <div class="card h-100">
-                    <img src="https://covers.openlibrary.org/b/id/${book.cover_i}.jpg" class="pt-3 card-img-top w-50 h-50 mx-auto" alt="...">
-                    <div class="card-body">
-                      <h3 class="card-title text-center">${book.title}</h3>
-                      <h6 class="card-text  text-center">${book.author_name[0]}.</h6>
-                      <h6 class="card-text  text-center">${book.publish_date[0]}.</h6>
-                    </div>
-                  </div>
-                </div>`;
-     bookContainer.appendChild(div);
-}
-}
+    bookContainer.textContent='';
+ 
+  
+      books.forEach(book=>{
+      
+          const div=document.createElement('div');
+          div.innerHTML=`
+                      <div class="col shadow-lg rounded">
+                        <div class="card h-100">
+                          <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="pt-3 card-img-top w-50 h-50 mx-auto" alt="...">
+                          <div class="card-body">
+                            <h3 class="card-title text-center">${book.title}</h3>
+                            <h6 class="card-text  text-center">Author:${book.author_name[0]?book.author_name[0]:''}.</h6>
+                            <h6 class="card-text  text-center">Publisher:${book.publisher[0]?book.publisher[0]:''}.</h6>
+                            <h6 class="card-text  text-center">${book.publish_date[0]?book.publish_date[0]:''}.</h6>
+                          </div>
+                        </div>
+                      </div>`;
+           bookContainer.appendChild(div);
+    })
+  }
+    
